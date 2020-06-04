1dd166fd4ba5b4070b053999bffef09410a0a53b
nearlyready <readyforchina@gmail.com>
2020-06-04 23:30:10 +0100

    Added music project.

---
+++ apps/music-maker/precache-manifest.472b8b595992be24acaa120e2c75a5c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c43d89fce8711c195d",
+    "url": "css/app.7a050125.css"
+  },
+  {
+    "revision": "8863ff977aee67e7eb60286a63fd38d5",
+    "url": "index.html"
+  },
+  {
+    "revision": "12c43d89fce8711c195d",
+    "url": "js/app.b336e455.js"
+  },
+  {
+    "revision": "6e10c38955bf7890b20c",
+    "url": "js/chunk-vendors.f152ed86.js"
+  },
+  {
+    "revision": "992604b381aa10c9b76675ba76031e04",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
