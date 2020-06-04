6ee486df8f26692beb49effc1a06e8f69601e4ba
nearlyready <readyforchina@gmail.com>
2020-06-04 13:36:10 +0100

    Another attempt at a simple PWA

---
+++ apps/pwa-test/precache-manifest.4c1b1a88940eaec20c4b59ae93801dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348dc126107637b50312",
+    "url": "/apps/pwa-test/css/app.e6826607.css"
+  },
+  {
+    "revision": "18b41d96b72ea9d5cceff1149d6cf56b",
+    "url": "/apps/pwa-test/index.html"
+  },
+  {
+    "revision": "348dc126107637b50312",
+    "url": "/apps/pwa-test/js/app.504cebd2.js"
+  },
+  {
+    "revision": "c1845060b5ede70dfb86",
+    "url": "/apps/pwa-test/js/chunk-vendors.21677903.js"
+  },
+  {
+    "revision": "b0d6ded906db037aaed0ff85b3eba813",
+    "url": "/apps/pwa-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/apps/pwa-test/robots.txt"
+  }
+]);
