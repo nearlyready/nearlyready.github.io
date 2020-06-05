3f7bc34df36210d4d96e61108b04397cf66d5b37
nearlyready <readyforchina@gmail.com>
2020-06-05 21:53:33 +0100

    Try again.

---
+++ apps/music-maker/precache-manifest.e60d82af197ba7214b3d76c699de29c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fff24a380c5afb0f8a",
+    "url": "css/app.86cd0420.css"
+  },
+  {
+    "revision": "601d72f2fed8ef5cc1d84bba228dba71",
+    "url": "index.html"
+  },
+  {
+    "revision": "98fff24a380c5afb0f8a",
+    "url": "js/app.56860c62.js"
+  },
+  {
+    "revision": "e9e68b6021007e3e8960",
+    "url": "js/chunk-vendors.d7bfaf12.js"
+  },
+  {
+    "revision": "992604b381aa10c9b76675ba76031e04",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
