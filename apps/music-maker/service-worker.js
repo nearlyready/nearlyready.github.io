4f00f4a258c0aa4f356154e9f5f38bf66dacdc42
nearlyready <readyforchina@gmail.com>
2020-06-19 20:22:20 +0100

    Adjust pwa caching.

--- apps/music-maker/service-worker.js
+++ apps/music-maker/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.dd699f27a69bdc6cdcc8ecfafee762b5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.c9b396cba00e5754b848f3f25dbb5c37.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
